fd72db933e619d2f01efee5a523475b7068790f7
oldmartijntje <oldmartijntje@gmail.com>
2022-11-14 21:11:28 +0100

    logging, start of create, more in interperator

--- advancepad.py
+++ advancepad.py
@@ -1,7 +1,20 @@
 import json
 import os
+import datetime
 
-baseInterperator = {"store": "dataStore"}
+# logging types:
+# none
+# all
+# error
+# modify
+# warning
+# deleteAfter
+
+maxPathLength = 10
+baseInterperator = {"storeData": ["dataStore"], "logging": {"where": ["logFiles"], "type": ["all"]},
+    "context": {"idNumber":0}, "library":"libraryContext"}
+logFile = ''
+basePath = ''
 
 def checkLoadSaveJson(location = '', data =''):
     if os.path.exists(f'{location}.json'):
@@ -18,6 +31,92 @@ def saveJson(location = '', data = ''):
     with open(f'{location}.json', 'w') as outfile:
         json.dump(data, outfile, indent=2)
 
+def logging(itemToLog: str = 'defaultValue', type = ''):
+    log = open(f'{logFile}', "a+")
+    log.write(f'{type}{itemToLog}\n\n')
+    log.close() 
+
+def checkLogging(logMessage: str = '', typeOfLog: str = 'default'):
+    if logMessage != '' and "none" not in interperator["logging"]["type"]:
+        if typeOfLog == 'default' and 'all' in interperator["logging"]["type"]:
+            logging(logMessage)
+        elif typeOfLog == 'warning' and ('all' in interperator["logging"]["type"] or 'warning' in interperator["logging"]["type"]):
+            logging(logMessage, '> Warning: ')
+        elif typeOfLog == 'error' and ('all' in interperator["logging"]["type"] or 'error' in interperator["logging"]["type"]):
+            logging(logMessage, '> Error: ')
+        elif typeOfLog == 'modify' and ('all' in interperator["logging"]["type"] or 'modify' in interperator["logging"]["type"]):
+            logging(logMessage, 'modified: ')
+        elif typeOfLog in interperator["logging"]["type"]:
+            logging(logMessage)
+
+
+def checkMakeFolderPath(givenPath: list = []):
+    if len(givenPath) > maxPathLength:
+        raise RecursionError(f"path exeeds the maximum amount of levels: {givenPath} whilst maximum is a level of {maxPathLength}")
+    currentPath = ''
+    for pathPart in givenPath:
+        if "/" in pathPart or "\\" in pathPart:
+            raise NameError(f"you can't put a '/' or '\\' in the path, the program does that automativcally.\n{pathPart} in {givenPath} after {currentPath}")
+        if currentPath != '':
+            currentPath = f"{currentPath}/{pathPart}"
+        else:
+            currentPath = pathPart
+        if not os.path.isdir(currentPath):
+            if logFile != '':
+                checkLogging(f"os.mkdir({currentPath}) = currentPath", 'modify')
+            os.mkdir(currentPath)
+    return currentPath
+
+def exitCode():
+    log = open(f'{logFile}', "r")
+    text = log.read()
+    log.close()
+    if text == '' or "deleteAfter" in interperator["logging"]["type"]:
+        os.remove(f'{logFile}')
+
+def createFile(name):
+    
+    def stringToAscii(seedString:str): #turns everything into ther ASCII value
+        seedList = []
+        for x in seedString:
+            seedList.append(ord(x))#change every character into its ASCII value
+        seedString = ''.join([str(elem) for elem in seedList])#add list together into string
+        seed = int(seedString)
+        return seed
+    
+
+
 interperator = checkLoadSaveJson('interperator',baseInterperator)
-if not os.path.isdir(interperator["store"]):
-    os.mkdir(interperator["store"])
+
+if not "logging" in interperator:
+    interperator["logging"] = baseInterperator["logging"]
+if not "where" in interperator["logging"]:
+    interperator["logging"]["where"] = baseInterperator["logging"]["where"]
+if not "type" in interperator["logging"]:
+    interperator["logging"]["type"] = baseInterperator["logging"]["type"]
+if "none" not in baseInterperator["logging"]["type"]:
+    logFile = checkMakeFolderPath(interperator["logging"]["where"])
+    logFile = f"{logFile}/log{datetime.datetime.now().strftime('%d_%m_%Y-%H_%M_%S')}.md"
+    log = open(logFile, "w")
+    log.close()  
+if not "storeData" in interperator:
+    interperator["storeData"] = baseInterperator["storeData"]
+    checkLogging(f"interperator[\"storeData\"] was not a thing, so we made it ```{interperator['storeData']}```", 'warning')
+basePath = checkMakeFolderPath(interperator["storeData"])
+if not "context" in interperator:
+    interperator["context"] = baseInterperator["context"]
+    checkLogging(f"interperator[\"context\"] was not a thing, so we made it ```{interperator['context']}```", 'warning')
+if not "library" in interperator:
+    interperator["library"] = baseInterperator["library"]
+    checkLogging(f"interperator[\"storeData\"] was not a thing, so we made it ```\"{interperator['library']}\"```", 'warning')
+if not os.path.isdir(interperator["library"]):
+    os.mkdir(interperator["library"])
+    checkLogging(f"os.mkdir({interperator['library']}) interperator['library']", 'modify')
+
+
+createFile('henk')
+
+
+
+# os.remove(f'interperator.json')
+exitCode()
